ff66839706ff4b051f386f1a5f621b84064adfc1
Selivanov <superexpulsion@mail.ru>
2018-05-04 14:00:41 +0300

    Сделал загрузку данных как в учебном

--- js/gallery.js
+++ js/gallery.js
@@ -16,12 +16,41 @@
     return photoElement;
   };
 
-  var fragment = document.createDocumentFragment();
-  // --------- функция заполнения фрагмента DOM-элементами на основе массива ---------
-  for (var i = 0; i < window.photos.length; i++) {
-    fragment.appendChild(renderPhoto(window.photos[i], i));
-  }
+  // var fragment = document.createDocumentFragment();
+  // // --------- функция заполнения фрагмента DOM-элементами на основе массива ---------
+  // for (var i = 0; i < window.photos.length; i++) {
+  //   fragment.appendChild(renderPhoto(window.photos[i], i));
+  // }
+  //
+  // window.gallery.appendChild(fragment);
+  var successHandler = function (photos) {
+    var fragment = document.createDocumentFragment();
+    window.photos = photos;
+    var countPhotos = 25;
 
-  window.gallery.appendChild(fragment);
+    for (var i = 0; i < countPhotos; i++) {
+      fragment.appendChild(renderPhoto(window.photos[i], i));
+    }
+    window.gallery.appendChild(fragment);
+  };
 
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 5; margin: 0 auto; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.display = 'flex';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.top = '25%';
+    node.style.alignItems = 'center';
+    node.style.justifyContent = 'center';
+    node.style.width = '50%';
+    node.style.height = '100px';
+    node.style.fontSize = '24px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(successHandler, errorHandler);
 })();
